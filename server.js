9db9b1fc919d7381318613b8c110eef98d7550e0
yli-yasir <35302828+yli-yasir@users.noreply.github.com>
2021-07-16 03:57:33 +0300

    use graphql github api

--- server.js
+++ server.js
@@ -1,32 +1,30 @@
 require('dotenv').config();
 const express = require('express');
-const { Octokit } = require("@octokit/rest");
+let { graphql: githubGraphql } = require("@octokit/graphql");
 
-const octokit = new Octokit({
-    userAgent: 'yli-backend',
-    auth: process.env.GITHUB_PERSONAL_ACCESS_TOKEN,
-});
+githubGraphql = githubGraphql.defaults({
+    headers: {
+      authorization: `Bearer ${process.env.GITHUB_PERSONAL_ACCESS_TOKEN}`,
+    },
+  });
 
 const app = express();
 
+app.use(express.json());
+
 // Normally defined as separate endpoints...
 // Since these will be doing essentially the same thing, they are combined as one.
-app.get('/contents/(:path)?', async (req, res, next) => {
+app.post('/github/graphql', async (req, res, next) => {
     try {
-        const { data } = await octokit.rest.repos.getContent({
-            owner: 'yli-yasir',
-            repo: 'yli-contents',
-            path: req.params.path
-        });
-        const contents = data.map(content => content.name);
-        res.json(contents);
+        const githubRes = await githubGraphql(req.body);
+        res.json(githubRes);
     }
-    catch(e){
-        e.status ? res.sendStatus(e.status) : next(e);
+    catch (e) {
+        console.log(e);
     }
 });
 
 
 
-app.listen(process.env.PORT, () => console.log('Server has started!'))
+app.listen(3000, () => console.log('Server has started!'))
 
